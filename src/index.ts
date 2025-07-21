ad03296fa3967a13746dc45595c7ed61cbfbddc6
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-07-21 16:21:31 +0900

    add initial symbol manipulations.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,34 @@
+/// Using a JIS based keyboard, so that the mapping symbols are different from US layout.
+/// Take a look at comments for the mapping corresponding.
+
 import {
-  layer,
   map,
-  NumberKeyValue,
   rule,
-  withMapper,
   writeToProfile,
 } from 'karabiner.ts'
+
+const symbolManipulators = [
+  // Numeric row
+  map('grave_accent_and_tilde').to('7', 'shift'), // ZenHan => '
+  map('2', 'shift').to('open_bracket'), // Shift + 2 => @
+  map('7', 'shift').to('international3', 'shift'), // Shift + 7 => |
+  map('0', 'shift').to('open_bracket', 'shift'), // Shift + 0 => `
+  map('hyphen').to('equal_sign'), // - => ^
+  // Top row
+  map('open_bracket').to('close_bracket'), // @ => [
+  map('close_bracket').to('non_us_pound'), // [ => ]
+  // Middle row
+  map('semicolon').to('hyphen'), // ; => -
+  map('non_us_pound').to('semicolon'), // ] => ;
+  // Bottom row
+  map('international1', 'shift').to('international3', 'option'), // Shift + _ => \
+]
 
 // ! Change '--dry-run' to your Karabiner-Elements Profile name.
 // (--dry-run print the config json into console)
 // + Create a new profile if needed.
-writeToProfile('--dry-run', [
-  // It is not required, but recommended to put symbol alias to layers,
-  // (If you type fast, use simlayer instead, see https://evan-liu.github.io/karabiner.ts/rules/simlayer)
-  // to make it easier to write '←' instead of 'left_arrow'.
-  // Supported alias: https://github.com/evan-liu/karabiner.ts/blob/main/src/utils/key-alias.ts
-  layer('/', 'symbol-mode').manipulators([
-    //     / + [ 1    2    3    4    5 ] =>
-    withMapper(['⌘', '⌥', '⌃', '⇧', '⇪'])((k, i) =>
-      map((i + 1) as NumberKeyValue).toPaste(k),
-    ),
-    withMapper(['←', '→', '↑', '↓', '␣', '⏎', '⇥', '⎋', '⌫', '⌦', '⇪'])((k) =>
-      map(k).toPaste(k),
-    ),
-  ]),
-
+writeToProfile('Basic Profile for Lenovo Trackpoint Keyboard 2 by Kamome283', [
   rule('Key mapping').manipulators([
-    // config key mappings
-    map(1).to(1)
+    ...symbolManipulators,
   ]),
 ])
