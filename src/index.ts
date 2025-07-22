d2e63f2ea2ea7d5eee0760e4cdf5b01b83eaf337
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-07-22 11:43:15 +0900

    Add a SandS like function to Mod keys.

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
 /// Take a look at comments for the mapping corresponding.
 
 import {
+  FromKeyParam,
   map,
-  rule,
-  writeToProfile,
+  rule, ToEvent, writeToProfile,
 } from 'karabiner.ts'
 
 const symbolManipulators = [
@@ -28,13 +28,34 @@ const symbolManipulators = [
   map('international1', 'shift').to('international3', 'option'), // Shift + _ => \
 ]
 
-const modifierManipulators = [
-  map('japanese_pc_nfer').to('left_command'), // MuHenkan => LeftCommand
-  map('japanese_pc_xfer').to('right_command'), // Henkan => RightCommand
-  map('japanese_pc_katakana').to('left_control', ["left_option", 'left_shift']), // Katakana => LeftControl + LeftOption + LeftShift
-  map('left_command').to('left_control'), // LeftCommand => LeftControl
-  map('print_screen').to('right_control'), // PrintScreen => RightControl
+function createModManipulator(
+  from: FromKeyParam,
+  to: ToEvent,
+  toIfAlone?: ToEvent
+) {
+  const manipulator = map(from, undefined, 'any')
+    .to({...to, lazy: true})
+  return toIfAlone ? manipulator.toIfAlone(toIfAlone) : manipulator
+}
+
+const modifierManipulatorsBase: Parameters<typeof createModManipulator>[] = [
+  ['spacebar', {key_code: 'left_shift'}, {key_code: 'spacebar'}], // Space => LeftShift with Space
+  ['japanese_pc_nfer', {key_code: 'left_command'}, {key_code: 'escape'}], // MuHenkan => LeftCommand with Escape
+  ['japanese_pc_xfer', {key_code: 'right_command'}, {key_code: 'return_or_enter'}], // Henkan => RightCommand with Return
+  ['japanese_pc_katakana',
+    {key_code: 'left_control', modifiers: ['left_option', 'left_shift']},
+    undefined], // Katakana => LeftControl + LeftOption + LeftShift
+  ['left_option', {key_code: 'left_option'}, {key_code: 'tab'}], // LeftOption => LeftOption with Tab
+  ['right_option',
+    {key_code: 'right_option'},
+    {key_code: 'left_arrow'}], // RightOption => RightOption with LeftArrow
+  ['left_command', {key_code: 'left_control'}, {key_code: '7', modifiers: ['shift']}], // LeftCommand => LeftControl with Shift + 7
+  ['print_screen',
+    {key_code: 'right_control'},
+    {key_code: 'right_arrow'}], // PrintScreen => RightControl with RightArrow
 ];
+
+const modifierManipulators = modifierManipulatorsBase.map((args) => createModManipulator(...args));
 
 const arrowManipulators = [
   map('grave_accent_and_tilde').to('up_arrow'), // ZenHan => UpArrow
