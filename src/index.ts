7842c8c5773125e77fe9de177d3846cf8443d24f
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-09-17 16:18:01 +0900

    update: バッククォートとパイプの位置を交換
    パイプよりもバッククォートの方が入力頻度が高いのでここに持ってくるようにした。
    ただバッククォートは全角半角でも使えるのでそっちを中指で押すようになるかも

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,8 @@ type Definition = [From: FromDefinition, To: ToDefinition, ToIfAlone?: ToDefinit
 const symbolDefinitions: Definition[] = [
   // Numeric row
   [['2', ['shift']], ['open_bracket']], // Shift + 2 => @
-  [['7', ['shift']], ['international3', ['shift']]], // Shift + 7 => |
-  [['0', ['shift']], ['open_bracket', ['shift']]], // Shift + 0 => `
+  [['7', ['shift']], ['open_bracket', ['shift']]], // Shift + 7 => `
+  [['0', ['shift']], ['international3', ['shift']]], // Shift + 0 => |
   [['hyphen'], ['equal_sign']], // - => ^
   [['hyphen', ['shift']], ['equal_sign', ['shift']]], // Shift + - => ~
   // Top row
