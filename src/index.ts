9d51a30894663b748cd449d5855d9559b1d82ab1
Tsubasa <26428614+kamome283@users.noreply.github.com>
2026-02-07 17:01:36 +0900

    refactor: マニピュレーターの作成とエントリーポイントの分離

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,6 @@
-import {
-  FromKeyParam,
-  FromModifierParam,
-  map,
-  mapPointingButton,
-  rule,
-  writeToProfile,
-} from "karabiner.ts";
-import {Definition, definitions} from "./definitions";
-
-type ManipulatorBuilder = ReturnType<typeof map>
-
-// 既存の実装を流用しているので、ShiftなしとShiftありのような1つの修飾子との衝突は解決できる。
-// ただ、修飾子無しとShiftありとOptionありのような2つ以上の衝突がある場合はうまく解決できないような気がする。
-function defsToManipulators(definitions: Definition[]): ManipulatorBuilder[] {
-  const groupedByFromKey: Map<FromKeyParam, [FromModifierParam: FromModifierParam | undefined, Definition: Definition][]> = new Map();
-  for (const def of definitions) {
-    const [from] = def;
-    const [fromKey, fromModifiers] = from;
-    let group = groupedByFromKey.get(fromKey);
-    if (!group) {
-      group = [];
-      groupedByFromKey.set(fromKey, group);
-    }
-    group.push([fromModifiers, def]);
-  }
-
-  const manipulators: ManipulatorBuilder[] = [];
-  for (const group of groupedByFromKey) {
-    const [_, modsAndDefs] = group;
-    const usedModifiers = modsAndDefs.flatMap(([fromModifier, _]) => fromModifier);
-    const allModifiers = ["shift", "option", "control", "command"] as const;
-    const unusedModifiers = allModifiers.filter(mod => !usedModifiers.includes(mod));
-    for (const [_, definition] of modsAndDefs) {
-      const [from, to, toIfAlone] = definition;
-      const [fromKey, fromModifiers] = from;
-      const baseManipulator = map(fromKey, fromModifiers, unusedModifiers).to(to[0], to[1], to[2]);
-      const manipulator = toIfAlone ? baseManipulator.toIfAlone(toIfAlone[0], toIfAlone[1], toIfAlone[2]) : baseManipulator;
-      manipulators.push(manipulator);
-    }
-  }
-  return manipulators;
-}
+import {mapPointingButton, rule, writeToProfile} from "karabiner.ts";
+import {definitions} from "./definitions";
+import {defsToManipulators} from "./manipulatorCreation";
 
 const manipulators = defsToManipulators(definitions);
 
