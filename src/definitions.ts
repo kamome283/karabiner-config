da14f80f2a47d1396abc8a0f8388e7dbbdc295c6
Tsubasa <26428614+kamome283@users.noreply.github.com>
2026-05-13 18:30:03 +0900

    update: US配列キーボードをベースにした配列

--- src/definitions.ts
+++ src/definitions.ts
@@ -15,25 +15,31 @@ export type Definition = [From: FromDefinition, To: ToDefinition, ToIfAlone?: To
 
 export const definitions: Definition[] = [
   // Numeric row
-  [["grave_accent_and_tilde"], ["equal_sign"]], // ZenHan => ^
-  [["grave_accent_and_tilde", "left_shift"], ["equal_sign", "shift"]], // Shift + ZenHan => ~
-  [["2", "left_shift"], ["semicolon", "shift"]], // Shift + 2 => +
-  [["0", ["left_shift"]], ["international3", ["shift"]]], // Shift + 0 => |
+  [["2", "left_shift"], ["open_bracket"]], // Shift + 2 => @
+  [["6", "left_shift"], ["equal_sign"]],
+  [["7", "left_shift"], ["6", "shift"]],
+  [["8", "left_shift"], ["quote", "shift"]],
+  [["9", "left_shift"], ["8", "shift"]],
+  [["0", "left_shift"], ["9", "shift"]],
+  [["hyphen"], ["semicolon", "shift"]],
+  [["hyphen", "left_shift"], ["international1"]],
   // Top row
   [["open_bracket"], ["close_bracket"]], // @ => [
   [["open_bracket", ["left_shift"]], ["close_bracket", ["shift"]]], // Shift + @ => {
   [["close_bracket"], ["non_us_pound"]], // [ => ]
   [["close_bracket", ["left_shift"]], ["non_us_pound", ["shift"]]], // Shift + [ => }
   // Middle row
-  [["caps_lock"], ["semicolon"]], // CapsLock => ;
-  [["caps_lock", "left_shift"], ["2", "shift"]], // CapsLock => "
-  [["semicolon"], ["hyphen"]], // ; => -
-  [["semicolon", "left_shift"], ["hyphen", "shift"]], // Shift + ; => =
-  [["non_us_pound"], ["open_bracket"]], // ] => @
-  [["non_us_pound", "left_shift"], ["open_bracket", "shift"]], // Shift + ] => `
+  [["caps_lock"], ["hyphen"]], // CapsLock => -
+  [["caps_lock", "left_shift"], ["hyphen", "shift"]], // CapsLock => =
+  [["semicolon", "left_shift"], ["quote"]], // Shift + ; => :
+  [["quote"], ["7", "shift"]], // : => '
+  [["quote", "left_shift"], ["2", "shift"]], // Shift + => "
+  [["non_us_pound"], ["open_bracket", "shift"]], // ] => `
+  [["non_us_pound", "left_shift"], ["equal_sign", "shift"]], // Shift + ] => ~
   // Bottom row
   [["left_shift"], ["delete_or_backspace"]],
-  [["international1", ["left_shift"]], ["international3", ["option"]]], // Shift + _ => \
+  [["international1"], ["international3", "option"]], // Shift + _ => \
+  [["international1", "left_shift"], ["international3", "shift"]], // Shift + _ => |
   // Modifier keys
   [["spacebar"], ["left_shift"], ["spacebar"]],
   [["japanese_pc_nfer"], ["right_command"], ["escape"]],
